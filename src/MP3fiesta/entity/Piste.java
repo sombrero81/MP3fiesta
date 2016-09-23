f891d3afd13b10a4f229e661ebb1859361f09633
sombrero81 <tead987@gmail.com>
2016-09-23 15:36:16 +0200

    23/sept/2016 15h30

---
+++ src/MP3fiesta/entity/Piste.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MP3fiesta.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author admin
+ */
+@Entity
+public class Piste implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @OneToMany(mappedBy ="piste" )
+    private List<Achat>achats=new ArrayList<>();
+    
+    @OneToMany(mappedBy ="piste" )
+    private List<Chart> charts=new ArrayList<>();
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Piste)) {
+            return false;
+        }
+        Piste other = (Piste) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MP3fiesta.entity.Piste[ id=" + id + " ]";
+    }
+    
+}
